128334e4759c17f65ea9a32baa96e38738019574
TachiKanade <sad361975@gmail.com>
2017-09-28 16:53:30 +0700

    Add file Python

---
+++ python.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+quest =input("What is your quest? ")
+color =input("What is your favorite color? ")
+
+print("So your name is ",name,", your quest ",quest,", your favorite color ",color, sep="")
+print("So your name is ",name,", your quest ",quest,", your favorite color ",color, sep="")
+print("So your name is ",name,", your quest ",quest,", your favorite color ",color, sep="")
